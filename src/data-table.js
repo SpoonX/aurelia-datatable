f19aee78895a10282e8def69b2f56a2b906211e3
Scrunshes <scrunshes@gmail.com>
2015-12-22 13:24:52 +0100

    chore(component): Adapation of the code

--- src/data-table.js
+++ src/data-table.js
@@ -1,9 +1,11 @@
 import {bindable, inject, computedFrom} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
+import {customElement} from 'aurelia-templating';
 
+@customElement('data-table')
 @inject(Router, Element)
 export class DataTable {
-@bindable repository;
+  @bindable repository;
 
   @bindable data;
 
@@ -23,11 +25,21 @@ export class DataTable {
 
   count = 0;
 
+  /**
+   * Calculate number of pages whenever 'count' or 'limit' is updated.
+   *
+   * @returns {number} page count
+   */
   @computedFrom('count', 'limit')
   get pageCount () {
     return Math.ceil(this.count / this.limit);
   }
 
+  /**
+   * Create an array of labels whenever 'columns' is updated.
+   *
+   * @returns {Array}
+   */
   @computedFrom('columns')
   get columnLabels () {
     let labelsRaw = this.columns.split(','),
@@ -53,17 +65,32 @@ export class DataTable {
     return labels;
   }
 
+  /**
+   * Create a new data-table element.
+   *
+   * @param {Router} Router
+   * @param {Element} element
+   */
   constructor (Router, element) {
     this.router  = Router;
     this.element = element;
   }
 
+  /**
+   * Reload the data for the data-table.
+   */
   reload () {
     this.updateRecordCount();
 
     return this.load(this.page);
   }
 
+  /**
+   * Set row data into 'this'.
+   *
+   * @param id
+   * @param data
+   */
   setRowData (id, data) {
     for (let i in this.data) {
       if (id !== this.data[i].id) {
@@ -78,45 +105,69 @@ export class DataTable {
     return this;
   }
 
+  /**
+   * Calls 'updateRecordCount()' and 'load() when the view is attached.
+   */
   attached () {
     this.updateRecordCount();
 
     return this.load();
   }
 
+  /**
+   * Calls the repository count function and set it.
+   */
   updateRecordCount () {
     this.repository.count()
       .then(res => this.count = res.count)
-  .catch(res => console.error(res));
+      .catch(res => console.error(res));
   }
 
+  /**
+   * Calls native router 'navigateToRoute()' function whenever 'selected()' is called.
+   */
   navigateTo (id) {
     this.router.navigateToRoute(this.route, {id: id});
   }
 
-  destroyRow (id) {
-    return this.element.dispatchEvent(new CustomEvent('destroyed', this.data.asObject()));
-  }
-
+  /**
+   * Return populated entries.
+   *
+   * @param {Array} row
+   * @returns {Array}
+   */
   populate (row) {
     return this.repository.getPopulatedEntity(row);
   }
 
+  /**
+   * Delete a row of entries either using provided custom function or build-in one.
+   *
+   * @param {Array} row
+   */
   doDelete (row) {
-    if (typeof this.delete === 'function') {
-      return this.delete(this.populate(row));
+    if (typeof this.destroy === 'function') {
+      return this.destroy(this.populate(row));
     }
 
     this.populate(row).destroy()
       .then(() => {
-      this.reload();
-    this.triggerEvent('deleted', row);
-  })
-  .catch(error => {
-      this.triggerEvent('exception', {on: 'delete', error: error});
-  });
+        this.reload();
+        this.triggerEvent('destroyed', row);
+      })
+      .catch(error => {
+        this.triggerEvent('exception', {
+          on    : 'delete',
+          error : error
+        });
+      });
   }
 
+  /**
+   * Update row of entries either using provided custom function or built-in one.
+   *
+   * @param {Array} row
+   */
   doUpdate (row) {
     if (typeof this.update === 'function') {
       return this.update(this.populate(row));
@@ -124,14 +175,19 @@ export class DataTable {
 
     this.populate(row).update()
       .then(() => {
-      this.reload();
-    this.triggerEvent('updated', row);
-  })
-  .catch(error => {
-      this.triggerEvent('exception', {on: 'update', error: error});
-  });
+        this.reload();
+        this.triggerEvent('updated', row);
+      })
+      .catch(error => {
+        this.triggerEvent('exception', {on: 'update', error: error});
+      });
   }
 
+  /**
+   * Will change url.
+   *
+   * @param row
+   */
   selected (row) {
     if (this.select) {
       return this.select(this.repository.getPopulatedEntity(row));
@@ -140,18 +196,36 @@ export class DataTable {
     return this.navigateTo(row.id);
   }
 
+  /**
+   * Convenient function to dispatch event.
+   *
+   * @param {string} event
+   * @param {Object|Array} payload
+   * @returns {boolean}
+   */
   triggerEvent (event, payload) {
     return this.element.dispatchEvent(new CustomEvent(event, payload));
   }
 
+  /**
+   * Load previous page.
+   */
   loadPrevious () {
     this.load(this.page - 1);
   }
 
+  /**
+   * Load next page.
+   */
   loadNext () {
     this.load(this.page + 1);
   }
 
+  /**
+   * Load a page
+   *
+   * @param {page} page number
+   */
   load (page) {
     if (page === 0) {
       return;
@@ -167,11 +241,11 @@ export class DataTable {
 
     this.repository.find({skip: skip, limit: this.limit, populate: false}, true)
       .then(result => {
-      this.page = page;
-    this.data = result;
-  })
-  .catch(error => {
-      console.error('Something went wrong.', error);
-  });
+        this.page = page;
+        this.data = result;
+      })
+      .catch(error => {
+        console.error('Something went wrong.', error);
+      });
   }
 }
