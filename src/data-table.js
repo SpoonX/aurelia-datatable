73ab50587ea9f9a9569e4bce28ecb90c1f90eaca
jeremyvergnas <jeremy.vergnas@viacesi.fr>
2016-05-11 15:07:11 +0200

    style(project): Make it more readable

--- src/data-table.js
+++ src/data-table.js
@@ -5,30 +5,130 @@ import {Statham} from 'json-statham';
 @customElement('data-table')
 @inject(Router, Element)
 export class DataTable {
-@bindable repository;
-
-  @bindable data;
-
-  @bindable route;
-
-  @bindable columns;
-
-  @bindable select;
-
-  @bindable destroy = null;
-
-  @bindable update = null;
-
-  @bindable sortable = null;
-
+  @bindable repository;
+  // String representing the column names
+  @bindable columns = '';
+  // Column used by default for search
   @bindable defaultColumn;
-
+  // Show the search field? (Optional attribut)
   @bindable searchable = null;
+  // Columns can be sorted? (Optional attribut)
+  @bindable sortable = null;
+  // Rows are editable? (Optional attribut)
+  @bindable update = null;
+  // Rows are removable? (Optional attribut)
+  @bindable destroy = null;
+  // Rows are selactable? (Optional attribut)
+  @bindable select;
+  @bindable data;
+  @bindable route;
 
   count = 0;
   columnsArray = [];
   sortingCriteria = {};
-  searchCriteria = {};
+  searchCriteria = {}
+
+  constructor(Router, element) {
+    this.router  = Router;
+    this.element = element;
+  }
+
+  attached() {
+    return this.load();
+  }
+
+  load() {
+    this.updateRecordCount();
+    let criteria = this.buildCriteria();
+    this.repository.find(criteria, true).then(result => {
+     this.data = result;
+    })
+    .catch(error => {
+      console.error('Something went wrong.', error);
+    });
+  }
+
+  buildCriteria() {
+    let criteria = {};
+
+    if (this.searchable !== null && Object.keys(this.searchCriteria).length ) {
+      let propertyName = Object.keys(this.searchCriteria)[0];
+      if (this.searchCriteria[propertyName]) {
+        criteria['where'] = {};
+        criteria['where'][propertyName] = {};
+        criteria['where'][propertyName]['contains'] = this.searchCriteria[propertyName];
+      }
+    }
+    if (this.sortable !== null && Object.keys(this.sortingCriteria).length ) {
+      let propertyName = Object.keys(this.sortingCriteria)[0];
+      if (this.sortingCriteria[propertyName]) {
+        criteria['sort'] = propertyName + ' ' + this.sortingCriteria[propertyName];
+      }
+    }
+    return criteria;
+  }
+
+  populate (row) {
+    return this.repository.getPopulatedEntity(row);
+  }
+
+  doDelete(row) {
+    if (typeof this.delete === 'function') {
+      return this.delete(this.populate(row));
+    }
+
+    this.populate(row).destroy()
+      .then(ah => {
+      this.load();
+      this.triggerEvent('deleted', row);
+    })
+    .catch(error => {
+      this.triggerEvent('exception', {on: 'delete', error: error});
+    });
+  }
+
+  doUpdate (row) {
+    if (typeof this.update === 'function') {
+      return this.update(this.populate(row));
+    }
+
+    this.populate(row).update()
+      .then(() => {
+      this.load();
+      this.triggerEvent('updated', row);
+    })
+    .catch(error => {
+      this.triggerEvent('exception', {on: 'update', error: error});
+    });
+  }
+
+  doSort(columnLabel) {
+    if (this.sortable === null || this.isObject(columnLabel.column)) {
+      return;
+    }
+
+    if (this.sortingCriteria[columnLabel.column]) {
+      this.sortingCriteria[columnLabel.column] = (this.sortingCriteria[columnLabel.column] === 'asc' ? 'desc' : 'asc');
+    }
+    else {
+      this.sortingCriteria = {};
+      this.sortingCriteria[columnLabel.column] = 'asc';
+    }
+
+    this.load();
+  }
+
+  doSearch(searchInput) {
+    if (this.searchable === null) {
+      return;
+    }
+
+    if (!(this.defaultColumn in this.searchCriteria)) {
+      this.searchCriteria = {};
+    }
+    this.searchCriteria[this.defaultColumn] = searchInput;
+    this.load();
+  }
 
   @computedFrom('columns')
   get columnLabels () {
@@ -74,17 +174,9 @@ export class DataTable {
     }
   }
 
-  constructor (Router, element) {
-    this.router  = Router;
-    this.element = element;
-  }
-
-  attached () {
-    return this.load();
-  }
-
-  navigateTo (id) {
-    this.router.navigateToRoute(this.route, {id: id});
+  triggerEvent (event, payload = {}) {
+    payload.bubbles = true;
+    return this.element.dispatchEvent(new CustomEvent(event, payload));
   }
 
   destroyRow (id) {
@@ -95,36 +187,6 @@ export class DataTable {
     return this.repository.getPopulatedEntity(row);
   }
 
-  doDelete (row) {
-    if (typeof this.delete === 'function') {
-      return this.delete(this.populate(row));
-    }
-
-    this.populate(row).destroy()
-      .then(() => {
-      this.load();
-    this.triggerEvent('deleted', row);
-  })
-  .catch(error => {
-      this.triggerEvent('exception', {on: 'delete', error: error});
-  });
-  }
-
-  doUpdate (row) {
-    if (typeof this.update === 'function') {
-      return this.update(this.populate(row));
-    }
-
-    this.populate(row).update()
-      .then(() => {
-      this.load();
-    this.triggerEvent('updated', row);
-  })
-  .catch(error => {
-      this.triggerEvent('exception', {on: 'update', error: error});
-  });
-  }
-
   selected (row) {
     if (this.select) {
       return this.select(this.repository.getPopulatedEntity(row));
@@ -133,67 +195,8 @@ export class DataTable {
     return this.navigateTo(row.id);
   }
 
-  triggerEvent (event, payload) {
-    payload.bubbles = true;
-    return this.element.dispatchEvent(new CustomEvent(event, payload));
-  }
-
-  load () {
-    let criteria = this.buildCriteria();
-    this.repository.find(criteria, true).then(result => {
-     this.data = result;
-    })
-    .catch(error => {
-      console.error('Something went wrong.', error);
-    });
-  }
-
-  buildCriteria() {
-    let criteria = {};
-
-    if (this.searchable !== null && Object.keys(this.searchCriteria).length ) {
-      let propertyName = Object.keys(this.searchCriteria)[0];
-      if (this.searchCriteria[propertyName]) {
-        criteria['where'] = {};
-        criteria['where'][propertyName] = {};
-        criteria['where'][propertyName]['contains'] = this.searchCriteria[propertyName];
-      }
-    }
-    if (this.sortable !== null && Object.keys(this.sortingCriteria).length ) {
-      let propertyName = Object.keys(this.sortingCriteria)[0];
-      if (this.sortingCriteria[propertyName]) {
-        criteria['sort'] = propertyName + ' ' + this.sortingCriteria[propertyName];
-      }
-    }
-    return criteria;
-  }
-
-  doSort(columnLabel) {
-    if (this.sortable === null || this.isObject(columnLabel.column)) {
-      return;
-    }
-
-    if (this.sortingCriteria[columnLabel.column]) {
-      this.sortingCriteria[columnLabel.column] = (this.sortingCriteria[columnLabel.column] === 'asc' ? 'desc' : 'asc');
-    }
-    else {
-      this.sortingCriteria = {};
-      this.sortingCriteria[columnLabel.column] = 'asc';
-    }
-
-    this.load();
-  }
-
-  doSearch(searchInput) {
-    if (this.searchable === null) {
-      return;
-    }
-
-    if (!(this.defaultColumn in this.searchCriteria)) {
-      this.searchCriteria = {};
-    }
-    this.searchCriteria[this.defaultColumn] = searchInput;
-    this.load();
+  navigateTo (id) {
+    this.router.navigateToRoute(this.route, {id: id});
   }
 
   displayValue (row, propertyName) {
